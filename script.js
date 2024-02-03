8ea781c0448720bd6fbf99c7800f0c92ffa086c1
priyankachaudhary1 <chypriya634@gmail.com>
2024-02-03 10:08:46 +0545

    i have add setup file for backgroud changer app

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript
+document.getElementById('changeColor').addEventListener('click', function() {
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
